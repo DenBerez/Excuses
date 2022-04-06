798be0cb048fcbc6e25c639e04c3abaf492149ea
Dennis Berezin <dennis.m.berezin@gmail.com>
2022-04-06 14:28:12 -0600

    Sanitized auth, decreased size

--- server/database/index.js
+++ server/database/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'dennis',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
